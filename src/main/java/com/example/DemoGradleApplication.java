927db0020e50f91a12cf1c126269280fc83c6dce
dschoi <dschoi@cds-PC.KONADEV.COM>
2015-11-24 13:41:51 +0900

    plugin ssh war add

--- src/main/java/com/example/DemoGradleApplication.java
+++ src/main/java/com/example/DemoGradleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoGradleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoGradleApplication.class, args);
+        SpringApplication.run(DemoGradleApplication.class, args); //dd
+
     }
 }
